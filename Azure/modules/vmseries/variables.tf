23ad46cc5a08226837943f65dad21435ed51bdce
Justin Priest <jmpriest91@gmail.com>
2024-12-29 20:52:35 -0500

    updating vm series image version to non deprecated version.

--- Azure/modules/vmseries/variables.tf
+++ Azure/modules/vmseries/variables.tf
@@ -167,7 +167,7 @@ variable "img_sku" {
 
 variable "img_version" {
   description = "VM-series PAN-OS version - list available for a default `img_offer` with `az vm image list -o table --publisher paloaltonetworks --offer vmseries-flex --all`"
-  default     = "10.1.0"
+  default     = "10.2.0"
   type        = string
 }
 
